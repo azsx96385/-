52d3e60fc733fd8870448e5e60974f03e58cb9f7
azsx96385 <azsx96385@gmail.com>
2019-04-12 17:19:57 +0800

    features| show all todo list

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //[引用之套件]
 const express = require("express");
 const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
 //[model]
 const Todo = require("./models/todo");
 //express server 設定與啟用
@@ -20,6 +21,39 @@ db.once("open", () => {
   console.log("mongodb connected!!");
 });
 
-app.get("/", (req, res) => {
-  res.send("today is friday");
+//handlebars 樣板引擎設定
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//[路由區]--------------------------------------
+app.get("/todos", (req, res) => {
+  //顯示首頁
+
+  Todo.find((err, todo) => {
+    res.render("index", { todos: todo });
+  });
+});
+
+app.get("/todos/new", (req, res) => {
+  //顯示新增頁面 | 使用get-req.query.name 抓值 - 丟給model 建立參數
+  res.send("這是 todo 新增頁面");
+});
+app.get("/todos/:id", (req, res) => {
+  //檢視單一todo資料
+  res.send("檢視單一 todo 資料");
+});
+
+app.get("/todos/:id/edit", (req, res) => {
+  //檢視-單一todo資料 編輯頁
+  res.send("檢視-單一todo資料 編輯頁");
+});
+
+app.post("/todos/:id/edit", (req, res) => {
+  //編輯 單一todo資料
+  res.send("編輯 單一todo資料");
+});
+
+app.post("/todos/:id/delete", (req, res) => {
+  //刪除單一todo 資料
+  res.send("刪除單一todo 資料");
 });
